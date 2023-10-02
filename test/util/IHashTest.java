3e5f73d076b987d7bc75ec563e2a0b57e34ac85d
Mariana Agudelo Salazar <magusa2004@gmail.com>
2023-10-01 22:36:00 -0500

    HashNode passed all test cases

--- test/util/IHashTest.java
+++ test/util/IHashTest.java
@@ -1,4 +1,9 @@
 package util;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class IHashTest {
+    //TODO: Add the test methods.}
+    //FIXME: HOW DO I TEST THIS?
+
+
 }
